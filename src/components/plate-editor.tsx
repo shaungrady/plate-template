13f843b68507b45dcf0748759bbb61d620e0b0e6
Shaun Grady <swgrady@amazon.com>
2024-02-14 17:07:08 -0800

    Install limited number of plugins
    npm install @udecode/plate-paragraph @udecode/plate-media @udecode/plate-basic-marks @udecode/plate-alignment
    
    npx @udecode/plate-ui@latest add image-element paragraph-element

--- src/components/plate-editor.tsx
+++ src/components/plate-editor.tsx
@@ -1,25 +1,54 @@
 'use client';
 
-import { createPlugins, Plate } from '@udecode/plate-common';
+import { withProps } from '@udecode/cn';
+import { createPlugins, Plate, PlateContent, PlateLeaf } from '@udecode/plate-common';
+import { createParagraphPlugin, ELEMENT_PARAGRAPH } from '@udecode/plate-paragraph';
+import { createImagePlugin, ELEMENT_IMAGE } from '@udecode/plate-media';
+import { createBoldPlugin, MARK_BOLD } from '@udecode/plate-basic-marks';
+import { createAlignPlugin } from '@udecode/plate-alignment';
+
+import { ImageElement } from '@/components/plate-ui/image-element';
+import { ParagraphElement } from '@/components/plate-ui/paragraph-element';
 
 import { Editor } from '@/components/plate-ui/editor';
 
-const plugins = createPlugins([], {
-  components: {},
-});
+const plugins = createPlugins(
+  [
+    createParagraphPlugin(),
+    createImagePlugin(),
+    createBoldPlugin(),
+    createAlignPlugin({
+      inject: {
+        props: {
+          validTypes: [
+            ELEMENT_PARAGRAPH,
+            // ELEMENT_H1, ELEMENT_H2, ELEMENT_H3
+          ],
+        },
+      },
+    }),
+  ],
+  {
+    components: {
+      [ELEMENT_IMAGE]: ImageElement,
+      [ELEMENT_PARAGRAPH]: ParagraphElement,
+      [MARK_BOLD]: withProps(PlateLeaf, { as: 'strong' }),
+    },
+  }
+);
 
 const initialValue = [
   {
-    id: 1,
+    id: '1',
     type: 'p',
-    children: [{ text: '' }],
+    children: [{ text: 'Hello, World!' }],
   },
 ];
 
 export function PlateEditor() {
   return (
     <Plate plugins={plugins} initialValue={initialValue}>
-      <Editor placeholder="Type your message here." />
+      <PlateContent />
     </Plate>
   );
 }
